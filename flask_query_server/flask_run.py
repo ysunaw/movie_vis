eccbf0b2b2c8f86bd3e950f6fc73678ac1fc475c
vicmaurer <vmaurer@connect.ust.hk>
2018-05-11 22:20:30 +0800

    added flask_query_server

---
+++ flask_query_server/flask_run.py
@@ -0,0 +1,57 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from load_tables import table_loader
+import json
+from flask import json
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+loader = table_loader()
+
+
+@app.route('/getColumnSize', methods = ['POST'])
+def api_message2():
+
+    if request.headers['Content-Type'] == 'application/json':
+        req = request.json
+        return loader.return_revenue_chart(req['num_columns'],req['Mean'])
+    else:
+        return "415 Unsupported Media Type ;)"
+
+
+@app.route('/getBubbleSizePosition', methods = ['POST'])
+
+def api_message():
+
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        req = request.json
+
+        return loader.return_filtered(req['start_t'],req['end_t'],req['max_num'],req['genres'])
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+    else:
+        return "415 Unsupported Media Type ;)"
+
+@app.route('/getAcstorNetwork', methods = ['POST'])
+def api_message3():
+
+    if request.headers['Content-Type'] == 'application/json':
+        req = request.json
+        return loader.return_actor_network(req['actor_id'])
+    else:
+        return "415 Unsupported Media Type ;)"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded = True)
